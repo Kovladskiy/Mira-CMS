73a3300b0904daa0b04927332ef50be56d59cde8
Kovladskiy <75542390+Kovladskiy@users.noreply.github.com>
2020-12-31 19:06:25 +0200

    Update Database.php

--- src/engine/plugins/system/Database.php
+++ src/engine/plugins/system/Database.php
@@ -8,8 +8,11 @@ class Database extends MiraCMS {
             $dbh = new PDO('mysql:host='.$db_server.';dbname='.$db_name.'', $db_username, $db_password);
             $this->db = $dbh;
         } catch (PDOException $e) {
+            $key = time();
+            $file =  __DIR__.'../../logs/Database_'.$key.'.miralogs';
+            $content = "Error:   ".$e->getMessage()."\n";
+            file_put_contents($file, $content, FILE_APPEND | LOCK_EX);
             http_response_code(500);
-            //print "Error!: " . $e->getMessage() . "<br/>";
         }
     }
     public function create() {
