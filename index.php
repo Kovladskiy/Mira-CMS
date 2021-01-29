47e52d0fbdeb16b09ed85489e64f36802748484b
Kovladskiy <75542390+Kovladskiy@users.noreply.github.com>
2021-01-29 21:03:49 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+require_once __DIR__.'/engine/Config.php';
+require_once __DIR__.'/engine/Core.php';
+
+set_error_handler(function($errno, $errstr, $errfile, $errline, $errcontext) {
+    if (0 === error_reporting()) {
+        return false;
+    }
+    throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+});
+
+try {
+    $cms_plugins = array();
+    $cms_core = new MiraCMS();
+    $cms_core->run();
+} catch (Exception $e) {
+    $date = time();
+    $full_date = date("y-m-d h:i:s",$date);
+    $date = date("y-m-d",$date);
+    $file = 'engine/logs/System_'.$date.'.txt';
+    $fh = fopen($file, 'a+') or die("Fatal Error!");
+    $logcontent = "Time : " . $full_date . "\r\n" . $e->getMessage() . "\r\n";
+    fwrite($fh, $logcontent);
+    fclose($fh);
+    http_response_code(500);
+}
+
+exit();
