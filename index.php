fe0767911249ddd65da27e7dc8f9a5c608cc2128
Kovladskiy <75542390+Kovladskiy@users.noreply.github.com>
2021-02-01 23:36:14 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+# Version: AlphaContavra  # 
+session_start();
+require_once 'engine/Config.php';
+require_once 'engine/Core.php';
+require_once 'engine/Database.php';
+
+
+set_error_handler(function($errno, $errstr, $errfile, $errline, $errcontext) {
+    if (0 === error_reporting()) {
+        return false;
+    }
+    throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+});
+
+try {
+    $cms_plugins = array();
+    $DB = new Database();
+    $cms_core = new MiraCMS();
+    $cms_core->run();
+} catch (Exception $e) {
+    $date = time();
+    $full_date = date("y-m-d h:i:s",$date);
+    $date = date("y-m-d",$date);
+    $file = 'engine/logs/System_'.$date.'.txt';
+    $fh = fopen($file, 'a+') or die("Fatal Error!");
+    $logcontent = "Time : " . $full_date . "\r\n" . $e->getMessage() . "\r\n";
+    fwrite($fh, $logcontent);
+    fclose($fh);
+    http_response_code(500);
+}
+
+exit();
+
