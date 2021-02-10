42b23c5dffd6c284638287b73b33dbdf6d94adc7
Kovladskiy <75542390+Kovladskiy@users.noreply.github.com>
2021-02-10 09:17:04 +0200

    update

--- engine/views/Admin/Home.php
+++ engine/views/Admin/Home.php
@@ -1,19 +1,3 @@
 <?php
     $cms_plugins['Template_Engine']->view('Home','Admin');
 ?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Source+Serif+Pro:400,600&display=swap" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="/templates/Admin/css/style.css">
-    <title>Admin Panel</title>
-</head>
-<body>
-
-</body>
-</html>
